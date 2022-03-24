6f3fb41ef2cb27eed5effdf3b9687adb67c9d2b5
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-03-24 10:18:49 +0400

    tak

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine(sentence.CustomContains(" "));
+
+        }
+    }
+}
